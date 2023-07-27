bce68b7a0577a0a83a537dd861e559d2066f8238
leonardorafaelli <leonardorafaelli@gmail.com>
2023-07-27 16:52:13 -0300

    finish layout and mvp

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using CommunityToolkit.Maui.Storage;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Storage;
 using Serilog;
+using System.Text.RegularExpressions;
 
 public partial class MainPage : ContentPage
 {
@@ -18,18 +19,36 @@ public partial class MainPage : ContentPage
     private FileSystemWatcher watcher;
     private string fileName, fileFullPath, contentType;
     private bool isWatchFolder;
+    private Timer watchingTxtTimer;
+    private Timer notificationTimer;
+    // Notification
+    private Entry mailEntry;
+    private string email = "";
+    private int valueResetTimer, seconds;
+
     // Setting options
     private bool isPremium = false;
     private string basepath;
+    private string premiumPassword = "root";
 
-    public string InputText { get; set; } = "";
     public MainPage()
     {
 
         try
         {
             InitializeComponent();
-            InputText = "";
+
+            mailEntry = new Entry
+            {
+                Keyboard = Keyboard.Email,
+                Placeholder = "E-mail",
+                WidthRequest = 200,
+                HeightRequest = 40,
+                TextColor = Color.FromRgb(0, 0, 0),
+                BackgroundColor = Color.FromRgb(238, 238, 238),
+            };
+
+            mailEntry.TextChanged += OnTextEmailChanged;
 
         }
         catch (Exception ex)
@@ -63,7 +82,7 @@ public partial class MainPage : ContentPage
                 Text = $"{folderFile}{e.ChangeType} - {currentDateTime}",
                 FontSize = 14,
                 HorizontalOptions = LayoutOptions.Start,
-                TextColor = Color.FromRgb(32,32,32)
+                TextColor = Color.FromRgb(32, 32, 32)
             };
 
             myLayout.Children.Add(label);
@@ -71,39 +90,79 @@ public partial class MainPage : ContentPage
             await monitorScroll.ScrollToAsync(0, Double.MaxValue, animated: true);
         });
     }
+
+    private void CleanShowOnScreen(Object sender, EventArgs e)
+    {
+        if (myLayout.Children.Count > 0)
+        {
+            myLayout.Clear();
+        }
+    }
+
+
     // ----
 
     private async void OnShowSettingsClicked(Object sender, EventArgs e)
     {
-        string setBasepath = "Set Basepath";
-        string bePremium = "Be Premium";
-        string premiumPassword = "teste";
 
-        string settingAction =  await DisplayActionSheet("Settings", null, "OK", setBasepath, bePremium);
-        
-        if(settingAction == bePremium)
+        //string setBasepath = "Set Basepath";
+        string bePremium = "Be Premium";
+        string resetPremium = "Reset Premium";
+        string changePremiumPass = "Change Premium Password";
+
+        string settingAction = await DisplayActionSheet("Settings", null, "Cancel", bePremium, changePremiumPass, isPremium ? resetPremium : null);
+
+        if (settingAction == bePremium)
         {
-            if(!isPremium)
+            if (!isPremium)
             {
                 string passResult = await DisplayPromptAsync("Be Premium", "Enter the password:");
 
-                if(passResult == premiumPassword)
+                if (passResult == premiumPassword)
                 {
                     loadingIcon.Source = "gold_loading_icon.png";
                     isPremium = true;
-                } else
+                }
+                else
                 {
                     await DisplayAlert("Wrong Password!", "You may talk to MR. Hollm", "OK");
                 }
-            } else
+            }
+            else
             {
                 await DisplayAlert("Be Premium", "You're already Premium", "OK");
             }
         }
 
-        if(settingAction == setBasepath)
+        //if (settingAction == setBasepath)
+        //{
+        //    string basepathResult = await DisplayPromptAsync("Set Basepath", "Enter the basepath you want the file/folder picker to start from:", "OK");
+        //}
+
+        if (settingAction == resetPremium)
         {
-            string basepathResult = await DisplayPromptAsync("Set Basepath", "Enter the basepath you want the file/folder picker to start from:", "OK");
+            string isSure = await DisplayActionSheet("Are you sure you want\nto reset Premium?", "No", "Yes");
+
+            if (isSure == "Yes")
+            {
+                loadingIcon.Source = "loading_icon.png";
+                isPremium = false;
+            }
+        }
+
+        if (settingAction == changePremiumPass)
+        {
+            string currPass = await DisplayPromptAsync("Change Premium Pass", "Enter the current password:", "OK", "Cancel");
+
+            if (currPass == premiumPassword)
+            {
+                string newPass = await DisplayPromptAsync("Change Premium Pass", "Enter the new password:", "OK", "Cancel");
+                premiumPassword = newPass;
+            }
+            else
+            {
+                await DisplayAlert("Wrong Password!", "You may talk to MR. Hollm", "OK");
+            }
         }
     }
 
@@ -114,7 +173,7 @@ public partial class MainPage : ContentPage
         {
             var folderResult = await FolderPicker.PickAsync(default);
 
-            if(folderResult.IsSuccessful)
+            if (folderResult.IsSuccessful)
             {
                 StopWatcher();
                 UpdateFileDetails(folderResult.Folder.Name, folderResult.Folder.Path, "folder");
@@ -122,9 +181,11 @@ public partial class MainPage : ContentPage
         }
         else
         {
-            FileResult file = await FilePicker.PickAsync(new PickOptions {
+            FileResult file = await FilePicker.Default.PickAsync(new PickOptions
+            {
                 PickerTitle = "Select file",
             });
+
 
             if (file != null)
             {
@@ -165,40 +226,6 @@ public partial class MainPage : ContentPage
         }
     }
 
-    private void OnNotifyClicked(Object sender, EventArgs e)
-    {
-        string smtpHost = "smtp.gmail.com";
-        int smtpPort = 587;
-        string emailRemetente = "diego_planinscheck@estudante.sc.senai.br"; // Substitua pelo seu e-mail do Gmail
-        string senhaRemetente = "phygbnrtzoinkdzw"; // Substitua pela sua senha do Gmail
-        string emailDestinatario = "leonardorafaelli@gmail.com"; // Substitua pelo e-mail do destinatário
-        string assunto = "Assunto do e-mail";
-        string corpoMensagem = "Corpo do pega no meu pau e-mail.";
-
-        try
-        {
-            using (var client = new SmtpClient(smtpHost, smtpPort))
-            {
-                // Autenticação no servidor SMTP
-                client.Credentials = new NetworkCredential(emailRemetente, senhaRemetente);
-                client.EnableSsl = true;
-
-                // Criar a mensagem do e-mail
-                var mensagem = new MailMessage(emailRemetente, emailDestinatario, assunto, corpoMensagem);
-
-                // Enviar o e-mail
-                client.Send(mensagem);
-
-                Log.Information("E-mail enviado com sucesso");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Erro ao enviar o e-mail: " + ex.Message);
-            Log.Information($"{ex.Message}");
-        }
-
-    }
 
     // Start monitoring changes on the selected file
     private void StartStopMonitor(object sender, EventArgs e)
@@ -210,16 +237,26 @@ public partial class MainPage : ContentPage
         if (watcher != null)
         {
             StopWatcher();
+            if (isPremium && watchingTxtTimer != null)
+            {
+                watchingTxt.Text = "Watching...";
+                watchingTxtTimer.Dispose();
+            }
 
         }
         else
         {
             StartWatcher();
+            if (isPremium)
+            {
+                StartWatchingTxtMovement();
+            }
         }
     }
 
     private void StopWatcher()
     {
+        StopNotifying();
         // Dispose the file Watcher, before creating a new one
         if (watcher != null)
         {
@@ -275,6 +312,184 @@ public partial class MainPage : ContentPage
         watcher.IncludeSubdirectories = true;
         watcher.EnableRaisingEvents = true;
 
+        // Start notification
+        StartNotifyTimer();
+
+    }
+
+    private void StartWatchingTxtMovement()
+    {
+
+        watchingTxtTimer = new Timer((Object state) =>
+        {
+            Dispatcher.Dispatch(() =>
+            {
+                string label1 = "Watching.", label2 = "Watching..", label3 = "Watching...";
+                if (watchingTxt.Text == label1)
+                {
+                    watchingTxt.Text = label2;
+                }
+                else if (watchingTxt.Text == label2)
+                {
+                    watchingTxt.Text = label3;
+                }
+                else
+                {
+                    watchingTxt.Text = label1;
+                }
+            });
+
+        }, null, 0, 1000);
+
+    }
+
+    public static bool IsValidEmail(string email)
+    {
+        // Regex pattern for basic email validation
+        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+        // Check if the email matches the pattern
+        return Regex.IsMatch(email, pattern);
+    }
+
+    // Notification
+    private void StartNotifyTimer()
+    {
+        if (notificationTimer != null)
+        {
+            notificationTimer.Dispose();
+            notificationTimer = null;
+        }
+        if (notifyMeCheckBox.IsChecked && seconds > 0)
+        {
+            notificationTimer = new Timer(SendNotification, null, seconds, Timeout.Infinite);
+        }
+    }
+
+    private void SendNotification(object state)
+    {
+
+        MainThread.InvokeOnMainThreadAsync(async () =>
+        {
+
+            string StopAll = "Stop All";
+            string StopNotification = "Stop Notification";
+
+            if (notifyAtCheckBox.IsChecked)
+            {
+                if (IsValidEmail(email))
+                {
+                    Task.Run(() => SendEmailNotification());
+                }
+            }
+            string result = await DisplayActionSheet("Notify me", null, "Continue", StopAll, StopNotification);
+
+            if (result == StopAll)
+            {
+                StopWatcher();
+            }
+            else if (result == StopNotification)
+            {
+                StopNotifying();
+            }
+            else
+            {
+                StartNotifyTimer();
+            }
+        });
+    }
+
+    private void StopNotifying()
+    {
+        if (notifyMeCheckBox.IsChecked)
+        {
+            notifyMeCheckBox.IsChecked = false;
+        }
+        if (notificationTimer != null)
+        {
+            notificationTimer.Dispose();
+            notificationTimer = null;
+        }
+    }
+
+    private void OnCheckBoxNotifyMeClicked(Object sender, CheckedChangedEventArgs e)
+    {
+        notifyMeCheckBox.IsChecked = e.Value;
+    }
+    private void OnCheckBoxAtClicked(Object sender, CheckedChangedEventArgs e)
+    {
+        notifyAtCheckBox.IsChecked = e.Value;
+        if (e.Value)
+        {
+
+            notificationLayout.Children.Add(mailEntry);
+        }
+        else
+        {
+            notificationLayout.Children.Remove(mailEntry);
+        }
+    }
+
+    private void OnTextEmailChanged(Object sender, TextChangedEventArgs e)
+    {
+        if (!IsValidEmail(e.NewTextValue))
+        {
+            mailEntry.TextColor = Color.FromRgb(220, 10, 10);
+        }
+        else
+        {
+            mailEntry.TextColor = Color.FromRgb(0, 0, 0);
+        }
+
+        // Text from email entry
+        email = e.NewTextValue;
+    }
+
+    private void OnTextSecondChanged(Object sender, TextChangedEventArgs e)
+    {
+        if (int.TryParse(e.NewTextValue, out int result))
+        {
+            seconds = result * 1000;
+            valueResetTimer = seconds;
+        }
+        else
+        {
+            ((Entry)sender).Text = null;
+        }
+
+    }
+
+    private void SendEmailNotification()
+    {
+        string smtpHost = "smtp.gmail.com";
+        int smtpPort = 587;
+        string emailRemetente = "watcherfile@gmail.com"; // Substitua pelo seu e-mail do Gmail
+        string senhaRemetente = "pufmraajecqvscgf"; // Substitua pela sua senha do Gmail
+        string emailDestinatario = email; // Substitua pelo e-mail do destinatário
+
+        string assunto = "Watcher notification";
+        string filefolder = isWatchFolder ? "folder" : "file";
+        string corpoMensagem = $"The {filefolder} you're watching has not received a changed within {seconds / 1000} seconds" +
+            $"\n\n\nIf you did not request a notification from the filewatcher.exe, please ignore this email.";
+
+        try
+        {
+            using (var client = new SmtpClient(smtpHost, smtpPort))
+            {
+                // Autenticação no servidor SMTP
+                client.Credentials = new NetworkCredential(emailRemetente, senhaRemetente);
+                client.EnableSsl = true;
+
+                // Criar a mensagem do e-mail
+                var mensagem = new MailMessage(emailRemetente, emailDestinatario, assunto, corpoMensagem);
+
+                // Enviar o e-mail
+                client.Send(mensagem);
+            }
+        }
+        catch (Exception ex)
+        {
+        }
 
     }
 
@@ -300,17 +515,6 @@ public partial class MainPage : ContentPage
             await img.RotateTo(deg, ms);
             img.Rotation = 0;
         }
-    }
-
-    // Initialize logs on a external file
-    private static void InitializeLogging()
-    {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Information()
-            .WriteTo.File(@"C:\Users\leona.RAFAELLINOTE\Desktop\logs.txt", rollingInterval: RollingInterval.Day)
-            .CreateLogger();
-
-        Log.Information("Logging initialized.");
     }
 
 }
